0a6e1c976fbf86c970f2de7ac59adc6215ef769f
Zahraa Bou Zeid <zahraahbouzeid@gmail.com>
2023-10-03 12:09:15 +0200

    Handle Keyboard Input

--- script.js
+++ script.js
@@ -67,6 +67,61 @@ function fctCalculate() {
     }
 }
 
+function fctOnKeyPress(event) {
+    const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
+    if (event.key in keys) {
+        if (equation.textContent == 0) 
+        {
+            equation.textContent = event.key;
+        }
+        else
+        {
+            equation.textContent += event.key;
+        }
+    } 
+    else if (event.key === "Enter")
+    {
+        fctCalculate();
+    }
+    else if (event.key === "Escape") 
+    {
+        fctClear();
+    } 
+    else if (event.key === "/") 
+    {
+        if (equation.textContent == 0) 
+        {
+            equation.textContent = "+";
+        }
+        else
+        {
+            equation.textContent += "+";
+        }
+    }
+    else if (event.key === "*") 
+    {
+        if (equation.textContent == 0) 
+        {
+            equation.textContent = "x";
+        }
+        else
+        {
+            equation.textContent += "x";
+        }
+    }
+    else if (event.key === "Delete" || event.key === "Backspace") 
+    {
+        fctDelete();
+    }
+    else if (event.keyCode === 187 || event.keyCode === 189 || event.keyCode === 48 || event.keyCode === 188 || event.keyCode === 57)
+    {
+        if (equation.textContent === "0") {
+            equation.textContent = event.key;
+        } else {
+            equation.textContent += event.key;
+        }
+    }
+}
 
 // EVENTS
 allClearButton.addEventListener("click", fctClear); // Clear All
@@ -78,3 +133,5 @@ equationButtons.forEach((button) => {
 });
 
 equalButton.addEventListener("click", fctCalculate); // Calculate
+
+document.addEventListener("keydown", fctOnKeyPress); // Handle Keyboard Input
