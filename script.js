fb9ea8099e07e1ac877c2f57096258ddbf6ddcf3
Zahraa Bou Zeid <zahraahbouzeid@gmail.com>
2023-10-03 13:12:19 +0200

    Refactoring:   script.js

--- script.js
+++ script.js
@@ -1,126 +1,89 @@
-var numberButtons = document.querySelectorAll('.number');
-var operatorButtons = document.querySelectorAll('.operator');
-var equationButtons = document.querySelectorAll('.equation');
-var equalButton = document.querySelector('.btn-equal');
-var deleteButton = document.querySelector('.btn-delete');
-var allClearButton = document.querySelector('.btn-all-clear');
-var parenthesesButton = document.querySelector('.btn-parentheses');
-var output = document.getElementById("output");
-var equation = document.getElementById("equation");
+//https://www.toptal.com/developers/keycode
+
+const output = document.getElementById("output");
+const equation = document.getElementById("equation");
+
+const allClearButton = document.querySelector('.btn-all-clear');
+const equalButton = document.querySelector('.btn-equal');
+const parenthesesButton = document.querySelector('.btn-parentheses');
+const deleteButton = document.querySelector('.btn-delete');
+const equationButtons = document.querySelectorAll('.equation');
+
 
 // FUNCTIONS
 function fctClear()
 {
-    output.textContent = "0"; 
-    equation.textContent = "0";
+    output.textContent = equation.textContent = "0";
 }
 
 function fctDelete()
 {
-    if (equation.textContent.length === 1)
-    {
-        fctClear(); 
-    }
-    else
-    {
+    if (equation.textContent.length === 1) {
+        fctClear();
+    } else {
         equation.textContent = equation.textContent.slice(0, -1);
     }
 }
 
-var parenthesesOpened = true;
-
-function fctAddToEquation(event) {
-    if ((equation.textContent == "0") && !(event.target.textContent == "( )"))
-    {
-        equation.textContent = event.target.textContent;
-    } 
-    else if ((event.target.textContent == "( )"))
-    {
-        if (equation.textContent == "0")
-        {
-            equation.textContent = "";  
-        }
-        if (parenthesesOpened) 
-        {
-            equation.textContent += "(";
-        } 
-        else 
-        {
-            equation.textContent += ")";
-        }
-        parenthesesOpened = !parenthesesOpened;
-    }
-    else 
-    {
-        equation.textContent += event.target.textContent;
-    } 
-}
-
 function fctCalculate() {
-    try
-    {
-        output.textContent = eval(equation.textContent.replace(/x/g, '*').replace(/÷/g, '/').replace(/,/g, '.'));
-    }
-    catch
-    {
+    try {
+        const result = eval(equation.textContent.replace(/x/g, '*').replace(/÷/g, '/').replace(/,/g, '.'));
+        output.textContent = result.toString().replace(/\./g, ',');
+    } catch {
         output.textContent = "Error";
     }
 }
 
-function fctOnKeyPress(event) {
-    const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
-    if (event.key in keys) {
-        if (equation.textContent == 0) 
-        {
-            equation.textContent = event.key;
+function fctAdjustEquation(input)
+{
+    if (equation.textContent == 0) {
+        equation.textContent = input;
+    } else {
+        equation.textContent += input;
+    }
+}
+
+function fctOnKeyPress(event) 
+{
+    const key = event.key;
+    const code = event.keyCode;
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
+    const operators = [187, 189, 48, 188, 57];
+    let operatorFound = false;
+    for(const operator of operators) {
+        if (code === operator) {
+            operatorFound = true;
         }
-        else
-        {
-            equation.textContent += event.key;
-        }
-    } 
-    else if (event.key === "Enter")
-    {
+    }
+
+    if (key in numbers) {
+        fctAdjustEquation(key);
+    } else if (key === "Enter") {
         fctCalculate();
-    }
-    else if (event.key === "Escape") 
-    {
+    } else if (key === "Escape") {
         fctClear();
-    } 
-    else if (event.key === "/") 
-    {
-        if (equation.textContent == 0) 
-        {
-            equation.textContent = "+";
-        }
-        else
-        {
-            equation.textContent += "+";
-        }
-    }
-    else if (event.key === "*") 
-    {
-        if (equation.textContent == 0) 
-        {
-            equation.textContent = "x";
-        }
-        else
-        {
-            equation.textContent += "x";
-        }
-    }
-    else if (event.key === "Delete" || event.key === "Backspace") 
-    {
+    } else if (key === "/") {
+        fctAdjustEquation("+");
+    } else if (key === "*") {
+        fctAdjustEquation("x");
+    } else if (key === "Delete" || event.key === "Backspace") {
         fctDelete();
+    } else if (operatorFound) {
+        fctAdjustEquation(key);
     }
-    else if (event.keyCode === 187 || event.keyCode === 189 || event.keyCode === 48 || event.keyCode === 188 || event.keyCode === 57)
-    {
-        if (equation.textContent === "0") {
-            equation.textContent = event.key;
-        } else {
-            equation.textContent += event.key;
+}
+
+let parenthesesOpened = true;
+function fctAddToEquation(event) {
+    if (event.target.textContent == "( )") {
+        if (equation.textContent == "0") {
+            equation.textContent = "";  
         }
-    }
+        equation.textContent += parenthesesOpened ? "(" : ")";
+        parenthesesOpened = !parenthesesOpened;
+        return;
+    } 
+    fctAdjustEquation(event.target.textContent);
 }
 
 // EVENTS
